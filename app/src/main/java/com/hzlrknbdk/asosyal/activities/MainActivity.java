674cfef0706318ac5cf90983ab8eee6b5c8d3b91
hzlrknbdk <hzlrknbdk@gmail.com>
2019-08-02 13:21:22 +0300

    Splash ekranından MainFragment'a geçiş yapıldı.
    Signed-off-by: hzlrknbdk <hzlrknbdk@gmail.com>

--- app/src/main/java/com/hzlrknbdk/asosyal/activities/MainActivity.java
+++ app/src/main/java/com/hzlrknbdk/asosyal/activities/MainActivity.java
@@ -1,23 +1,52 @@
 package com.hzlrknbdk.asosyal.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.hzlrknbdk.asosyal.R;
+import com.hzlrknbdk.asosyal.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lottieAnimationView = findViewById(R.id.splash_animation);
+
+        SplashAnimation();
+        pushFragment();
+
+    }
+
+    private void pushFragment() {
+
+        int SPLASH_TIME = 4000;
+        new Handler().postDelayed(() -> {
+            lottieAnimationView.setVisibility(View.GONE);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            Fragment mainFragment = new MainFragment();
+            transaction.replace(R.id.container, mainFragment);
+            transaction.commit();
+
+        }, SPLASH_TIME);
+    }
+
+    public void SplashAnimation() {
+
         LottieAnimationView animationView = findViewById(R.id.splash_animation);
-        animationView.setAnimation("preloader2.json");
+        animationView.setAnimation("preloader.json");
         animationView.playAnimation();
+
 
     }
 
