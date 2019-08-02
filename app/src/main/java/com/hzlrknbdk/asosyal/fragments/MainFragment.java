c7a4842aa82d89678111d2add3ec20c150f67d92
hzlrknbdk <hzlrknbdk@gmail.com>
2019-08-02 22:06:04 +0300

    MainFragment'taki butonlar ile diğer Fragmentlara geçiş yapıldı.
    Signed-off-by: hzlrknbdk <hzlrknbdk@gmail.com>

--- app/src/main/java/com/hzlrknbdk/asosyal/fragments/MainFragment.java
+++ app/src/main/java/com/hzlrknbdk/asosyal/fragments/MainFragment.java
@@ -6,28 +6,70 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.hzlrknbdk.asosyal.R;
 
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
 
     public MainFragment() {
 
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        view.findViewById(R.id.btn_social).setOnClickListener(this);
+        view.findViewById(R.id.btn_parents).setOnClickListener(this);
+        view.findViewById(R.id.btn_teacher).setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_social:
+                SocialFragment socialFragment = new SocialFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                assert fragmentManager != null;
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container, socialFragment);
+                //   fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+
+            case R.id.btn_parents:
+                ParentsFragment parentsFragment = new ParentsFragment();
+                FragmentManager fragmentManager1 = getFragmentManager();
+                assert fragmentManager1 != null;
+                FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
+                fragmentTransaction1.replace(R.id.container, parentsFragment);
+                //    fragmentTransaction1.addToBackStack(null);
+                fragmentTransaction1.commit();
+
+            case R.id.btn_teacher:
+                TeacherFragment teacherFragment = new TeacherFragment();
+                FragmentManager fragmentManager2 = getFragmentManager();
+                assert fragmentManager2 != null;
+                FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
+                fragmentTransaction2.replace(R.id.container, teacherFragment);
+                //   fragmentTransaction2.addToBackStack(null);
+                fragmentTransaction2.commit();
+        }
+
+    }
 }
